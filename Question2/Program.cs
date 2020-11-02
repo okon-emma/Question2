1dbd720c682f867259cdcbec390e58ae11e5d0e2
Okon Emmanuel <drefiom@gmail.com>
2020-11-02 10:50:51 +0100

    first commit

---
+++ Question2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayInput = new string[9]
+            {
+                "ZURICH", "ABU DHABI", "AMSTERDAM", "CALIFORNIA", "NEW DELHI",
+                "PARIS", "LONDON", "ROME", "NAIROBI"
+            };
+
+            IOrderedEnumerable<string> orderedInput = arrayInput.OrderBy(i => i.Length);
+            IOrderedEnumerable<string> orderedInput2 = orderedInput.ThenBy(i => i.First());
+            List<string> listInput = orderedInput2.ToList();
+
+            foreach(string item in listInput)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
